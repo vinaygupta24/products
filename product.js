927db82b5a2f7dd3efe03beff0890654a4f6399b
Vinay <guptavinay0289@gmail.com>
2023-05-21 07:55:06 +0530

    Formatting the code...

--- product.js
+++ product.js
@@ -2,42 +2,42 @@ const productDOM = document.querySelector('.product');
 const url = 'https://course-api.com/javascript-store-single-product';
 
 const fetchProduct = async () => {
-  try {
-    productDOM.innerHTML = '<h4 class="product-loading">Loading... </h4>';
-    // console.log(window.location.search);
-    const params = new URLSearchParams(window.location.search);
-    const id = params.get('id');
+    try {
+        productDOM.innerHTML = '<h4 class="product-loading">Loading... </h4>';
+        // console.log(window.location.search);
+        const params = new URLSearchParams(window.location.search);
+        const id = params.get('id');
 
-    const response = await fetch(`${url}?id=${id}`);
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    productDOM.innerHTML =
-      '<p class="error">There was a problem loading the product. Please try again later </p>';
-  }
+        const response = await fetch(`${url}?id=${id}`);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        productDOM.innerHTML =
+            '<p class="error">There was a problem loading the product. Please try again later </p>';
+    }
 };
 
 const displayProduct = (product) => {
-  // company, colors, description, name:title, price, image(url:img)
-  const {
-    company,
-    colors,
-    description,
-    name: title,
-    price,
-    image,
-  } = product.fields;
-  const { url: img } = image[0];
-  document.title = title.toUpperCase();
+    // company, colors, description, name:title, price, image(url:img)
+    const {
+        company,
+        colors,
+        description,
+        name: title,
+        price,
+        image,
+    } = product.fields;
+    const { url: img } = image[0];
+    document.title = title.toUpperCase();
 
-  // colors
-  const colorsList = colors
-    .map((color) => {
-      return `<span class="product-color" style="background: ${color}"></span>`;
-    })
-    .join('');
+    // colors
+    const colorsList = colors
+        .map((color) => {
+            return `<span class="product-color" style="background: ${color}"></span>`;
+        })
+        .join('');
 
-  productDOM.innerHTML = `<div class="product-wrapper">
+    productDOM.innerHTML = `<div class="product-wrapper">
         <img src="${img}" class="img" alt="${title}" />
         <div class="product-info">
           <h3>${title}</h3>
@@ -56,8 +56,8 @@ const displayProduct = (product) => {
 };
 
 const start = async () => {
-  const data = await fetchProduct();
-  displayProduct(data);
+    const data = await fetchProduct();
+    displayProduct(data);
 };
 
 start();
